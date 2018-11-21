804344e9bce80ef66f538ae775a90e0db2dcc501
antonio craveiro <pe_1992@hotmail.com>
2018-11-21 12:47:01 +0100

    Added a compare and a maximum method

--- cpp-formation-playground/Point.cpp
+++ cpp-formation-playground/Point.cpp
@@ -1,11 +1,43 @@
 #include "pch.h"
 #include "Point.h"
 #include <iostream>
+#include <string>
+
+unsigned int Point::counter = 0;
+
+
+std::string compareValueToString(CompareValue enumValue) {
+	switch (enumValue)
+	{
+	case 1:
+		return "greater than";
+	case -1:
+		return "lesser than";
+	default:
+		return "equal to";
+	}
+}
+
+Point::Point() : Point(0, 0)
+{
+}
+
+Point::Point(float x) : Point(x,0)
+{
+}
 
 Point::Point(float x, float y)
 {
 	this->x = x;
 	this->y = y;
+	Point::counter++;
+}
+
+Point::Point(const Point & point)
+{
+	x = point.x;
+	y = point.y;
+	Point::counter++;
 }
 
 void Point::move(float x, float y)
@@ -14,11 +46,35 @@ void Point::move(float x, float y)
 	this->y = y;
 }
 
-void Point::display()
+void Point::display() const
 {
 	std::cout << "The coordinates of the point are x: " << x << " and y: " << y << std::endl;
 }
 
+unsigned int Point::numberOfPointers()
+{
+	return counter;
+}
+
+CompareValue Point::compare(const Point &p)
+{
+	if (x + y < p.x + p.y)
+		return Bigger;
+	else if (x + y > p.x + p.y)
+		return Smaller;
+	return Equal;
+}
+
+Point Point::maximum(const Point &p)
+{
+	CompareValue compareValue = compare(p);
+	if (compareValue == 1)
+		return p;
+	else
+		return *this;
+}
+
 Point::~Point()
 {
+	Point::counter--;
 }
--- cpp-formation-playground/Point.h
+++ cpp-formation-playground/Point.h
@@ -1,13 +1,30 @@
+#include <string>
 #pragma once
+
+enum CompareValue
+{
+	Bigger = 1,
+	Equal = 0,
+	Smaller = -1
+};
+
+std::string compareValueToString(CompareValue);
 
 class Point
 {
 	float x;
 	float y;
+	static unsigned int counter;
 public:
+	Point();
+	Point(float);
 	Point(float,float);
+	Point(const Point&);
 	void move(float, float);
-	void display();
+	void display() const ;
+	static unsigned int numberOfPointers();
+	CompareValue compare(const Point&);
+	Point maximum(const Point&);
 	~Point();
 };
 
